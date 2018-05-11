d792e123034e699b99c676c14a7d5df56a1ee747
Juan Mendieta <juanmsn@live.com>
2018-05-11 13:34:29 -0500

    Borrado de claves personales de Sendgrid

--- services/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define("SENDGRID_API_KEY", "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");
-    define("TO_NAME", "Juan Mendieta");
-    define("TO_EMAIL", "juanmsn@live.com");	
