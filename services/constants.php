711c60ca05032c666fad0a362adc4a03785cdf90
Juan Mendieta <juanmsn@live.com>
2018-05-11 19:03:57 -0500

    remove constants.php

--- services/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define("SENDGRID_API_KEY", "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");
-    define("TO_NAME", "Juan Mendieta");
-    define("TO_EMAIL", "juanmsn@live.com");	
